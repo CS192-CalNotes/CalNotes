ed835795bed3108d5eeb3f43f1414627e6f7ee31
denisemariel <mmgapuz@up.edu.ph>
2021-03-15 23:49:43 +0800

    Initialized layout; default view is task view

---
+++ CalNotes/calnote/static/calnote/sidebar.js
@@ -0,0 +1,7 @@
+function opensidebar() {
+    document.getElementById("collapsible-sidenav").style.width = "250px";
+  }
+  
+function closesidebar() {
+    document.getElementById("collapsible-sidenav").style.width = "0";
+}
