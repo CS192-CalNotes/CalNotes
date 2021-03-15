6ca02d459fe1746d269d02e94000df21f894477a
Eunice Ceniza <lgceniza@up.edu.ph>
2021-03-15 19:57:25 +0800

    created task model

--- CalNotes/calnote/models.py
+++ CalNotes/calnote/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from datetime import datetime
+
+class Task(models.Model):
+	taskID = models.AutoField(primary_key = True)
+	task = models.CharField(max_length = 100)
+	dueDate = models.DateTimeField(default = datetime.now, blank = True)
+	isComplete = models.BooleanField(default = False)
+
+	def __str__(self):
+		return self.task
