5ee8a1dbadd65373a54408a2c39c4136efb8aeb8
erwinlucas <uruki.genbu@gmail.com>
2021-03-20 16:09:17 +0800

    Added AddTaskForm form
    Added AddTaskForm form with widgets and fields.

--- CalNotes/calnote/forms.py
+++ CalNotes/calnote/forms.py
@@ -1,3 +1,18 @@
 from django import forms
+from .models import Task #class Task from models.py
 
 # Insert form classes here
+class AddTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['task','dueDate']
+        widgets = {
+            'task': forms.TextInput(
+                attrs={
+                    'class':'form-control',
+                    'placeholder':'Enter task here',
+                    'aria-label':'Task',
+                    'aria-describedby':'add-btn'}
+            ),
+            'dueDate': forms.DateTimeField()
+        }
--- CalNotes/calnote/views.py
+++ CalNotes/calnote/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+
+from .models import Task #imports class Task from models.py
+from .forms import AddTaskForm #imports class AddTaskForm from forms.py
 
 # Create your views here.
 
 def index(request):
-    return render(request, "calnote/index.html")
+    task_list = Task.objects.order_by('taskID')
+    addtaskform = AddTaskForm()
+    context = {'task_list': task_list, 'form': addtaskform}
+    return render(request, "calnote/index.html", context)
+
+@require_POST
+def addTask(request):
+    addtaskform = AddTaskForm(request.POST)
+    if addtaskform.is_valid():
+        new_addtask = addtaskform.save() #new addtask object
+    return redirect('index')
