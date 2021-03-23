e60d57a9ff11440925316e26b4a61b92e828a661
Justine Romero <thepiesaresquared@gmail.com>
2021-03-23 18:05:36 +0800

    fix: timezone aware date

--- CalNotes/calnote/models.py
+++ CalNotes/calnote/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
 
 class Task(models.Model):
 	taskID = models.AutoField(primary_key = True)
 	task = models.CharField(max_length = 100)
-	dueDate = models.DateTimeField(default = datetime.now, blank = True)
+	dueDate = models.DateTimeField(default = timezone.now, blank = True)
 	isComplete = models.BooleanField(default = False)
 
 	def __str__(self):
