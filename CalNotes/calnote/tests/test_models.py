0ad881dde290fa4d48625953edbc8200a0376046
Justine Romero <thepiesaresquared@gmail.com>
2021-03-23 18:17:29 +0800

    chore: organize files

---
+++ CalNotes/calnote/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from calnote.models import Task
+from faker import Faker
+from django.utils import timezone
+from random import randint
+
+# Start a faker instance
+fake = Faker()
+
+class TaskModelTest(TestCase):
+ 
+    def create_task(self, name, date, isComplete):
+        """Utility function for creating task."""
+        Task.objects.create(task=name,dueDate=date,isComplete=isComplete)
+
+    def test_create_task(self):
+        """Test for task model creation."""
+        fakeName = fake.text()[0:100]
+        date = timezone.now()
+        isComplete = (True,False)[randint(0,1)]
+
+        self.create_task(fakeName,date,isComplete)
+
+        task = Task.objects.get(task=fakeName)
+        self.assertTrue(isinstance(task,Task))
+        self.assertNotEqual(getattr(task,"taskID",False),False)
+        self.assertEqual(task.task,fakeName)
+        self.assertEqual(task.dueDate.timestamp(), date.timestamp())
+        self.assertEqual(task.isComplete,isComplete)
--- CalNotes/calnote/tests/test_views.py
+++ CalNotes/calnote/tests/test_views.py
@@ -1,8 +1,5 @@
-from django.test import TestCase
-from calnote.models import Task
 from faker import Faker
 from django.utils import timezone
-from random import randint
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -10,35 +7,7 @@ from selenium.webdriver.remote.remote_connection import LOGGER, logging
 from selenium.webdriver.chrome.options import Options
 import time
 
-# Set selenium verbosity to minimum
-LOGGER.setLevel(logging.WARNING)
-
-# Start a faker instance
 fake = Faker()
-
-class TaskModelTest(TestCase):
-
- 
-    def create_task(self, name, date, isComplete):
-        """Utility function for creating task."""
-        Task.objects.create(task=name,dueDate=date,isComplete=isComplete)
-
-    def test_create_task(self):
-        """Test for task model creation."""
-        fakeName = fake.text()[0:100]
-        date = timezone.now()
-        isComplete = (True,False)[randint(0,1)]
-
-        self.create_task(fakeName,date,isComplete)
-
-        task = Task.objects.get(task=fakeName)
-        self.assertTrue(isinstance(task,Task))
-        self.assertNotEqual(getattr(task,"taskID",False),False)
-        self.assertEqual(task.task,fakeName)
-        self.assertEqual(task.dueDate.timestamp(), date.timestamp())
-        self.assertEqual(task.isComplete,isComplete)
-
-  
 
 class TaskViewTest(StaticLiveServerTestCase):
     def setUp(self):
